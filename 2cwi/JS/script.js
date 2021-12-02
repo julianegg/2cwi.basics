d0f1dd64d6dfb0a3742f13bd18c5fa8fc5b083bf
Julian <julian.deronja@student.htldornbirn.at>
2021-12-02 14:13:56 +0100

    JS first video done

---
+++ 2cwi/JS/script.js
@@ -0,0 +1,26 @@
+console.log("hallo");
+console.log("Tolle sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("A hat den wert " + a);
+
+
+
+console.log("a + b ");
+
+//rechnungsarten 
+// *, /, +, -
+
+
+
+
+
+Ergebnis = a + b + c;
+console.log("a + b + c = " + Ergebnis);
+
+Ergebnis = c - a + b;
+console.log("c - a + b =" + Ergebnis)
