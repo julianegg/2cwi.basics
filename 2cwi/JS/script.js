5ed066416609dc78e212f9b2ad434ebc1d477775
Julian <julian.deronja@student.htldornbirn.at>
2021-12-09 14:43:03 +0100

    js exercises

--- 2cwi/JS/script.js
+++ 2cwi/JS/script.js
@@ -19,26 +19,66 @@ console.log("A hat den wert " + a);
 
 
 
-Ergebnis = a + b + c;
+let Ergebnis = a + b + c;
 console.log(Ergebnis);
 
-Ergebnis = c - a + b;
-console.log(Ergebnis)
+let Ergebnis5 = c - a + b;
+console.log(Ergebnis5)
 
 
 let d = 7.4;
 let e = 7.5;
 
-Ergebnis = d + e;
-console.log(Ergebnis)
+let Ergebnis0 = d + e;
+console.log(Ergebnis0)
 
 //Texte STRINGS
 
 let Name = "juli";
 console.log(Name);
 
-Ergebnis = a + b;
-console.log("das ergebnis von " + a + " und " + b + " ist " + Ergebnis);
+Ergebnis1 = a + b;
+console.log("das ergebnis von " + a + " und " + b + " ist " + Ergebnis1);
 
-nic = d - e;
-console.log("das ergebnis von " + d + " und " - e + "  ist " + nic);
+result1 = (e - d);
+console.log("das ergebnis von " + e + " und " + d + "  ist " + result1);
+
+result2 = (c - b) * a;
+console.log(result2)
+
+// AUfgabe zu Variablen
+let banane = "Banane";
+let apple = "Apple";
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let Kilo = 1;
+let pbanane = 0.22;
+let papple = 0.34;
+
+//Apfel 
+let result5 = (Kilo / papple);
+console.log(" Ein Kilo sind " + result5 + " Äpfel ")
+
+let result6 = (applePricePerKilo / result5);
+console.log(" ein apfel kostet " + result6)
+
+let result7 = (result6 * 8);
+console.log(" acht äpfel kosten " + result7)
+
+let result11 = (applePricePerKilo * 1000);
+console.log(" eine tonne Äpfel kostet " + result11)
+
+//Banane 
+
+let result8 = (Kilo / pbanane);
+console.log(" Ein Kilo sind " + result8 + " Bananen ")
+
+let result9 = (bananaPricePerKilo / result8);
+console.log(" Eine Banane kostet " + result9)
+
+let result10 = (result9 * 17)
+console.log(" 17 bananen Kosten " + result10)
+
+let result12 = (bananaPricePerKilo * 1000);
+console.log(" Ein Kilo Bananen Koste " + result12)
