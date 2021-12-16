2db174c7cf63d3aa9eaff062ebe0bec2dd81cd69
Julian <julian.deronja@student.htldornbirn.at>
2021-12-16 14:53:37 +0100

    js

---
+++ 2cwi/JS/script2.js
@@ -0,0 +1,40 @@
+console.log("test");
+
+
+for (let count = 10; count >= 0; count--) {
+    console.log("Julian");
+}
+
+// AUFGABEN VIDEO 04
+console.log("Zähle von 7 bis 45");
+for (let count = 7; count <= 45; count++) {
+    console.log(count);
+
+}
+console.log("zähle von 9 bis 99");
+for (count = 9; count <= 99; count++) {
+    console.log(count);
+}
+console.log("zähle rückwerts von 10 bis 0");
+for (let count = 10; count >= 0; count--) {
+    console.log(count);
+}
+console.log("zähle rückwerts von 1234 bis 1207");
+
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count);
+}
+console.log("gib deinen namen genau 8 mal aus");
+for (let count = 1; count <= 8; count++) {
+    console.log("juli" + count);
+}
+console.log("Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von zehn auf null");
+for (let count = 0; count <= 10; count++) {
+    console.log(count);
+}
+
+console.log("julian");
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count);
+}
