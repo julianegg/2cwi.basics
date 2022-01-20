f4f0ba8283f8cccbc45511a322d9cf25da6e3b2b
Julian <julian.deronja@student.htldornbirn.at>
2022-01-20 14:24:13 +0100

    test

---
+++ 2cwi/JS/Test2cWi/script5.js
@@ -0,0 +1,14 @@
+console.log("Ich zähle von 1 bis 8");
+for (let count = 1; count <= 8; count++) {
+
+    console.log(count + " ")
+
+}
+
+console.log("Das wars!")
+
+console.log("Ich zähle in zweierschritten rückwerts");
+for (let count = 11; count >= -3; count = count - 2) {
+    console.log(count);
+
+}
