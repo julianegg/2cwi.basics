78e945feb5ac13310f6e2282fd57ea45e09554ad
Julian <julian.deronja@student.htldornbirn.at>
2022-03-03 14:41:11 +0100

    aufgabe09

--- 2cwi/JS/script7.js
+++ 2cwi/JS/script7.js
@@ -1,40 +1,40 @@
-//Aufgabe 09
+// Aufgabe 09
 let output = "";
-
 let isX = true
-
-for (let index = 1; index < 20; index++) {
-
+for (let index = 0; index < 20; index++) {
     if (index % 4 == 0) {
-
         console.log(output);
-
         output = ""
-
-        if (isX = true) {
-
+        if (isX == true) {
             isX = false
-
         } else {
-
-            isX = true
-
+            isX = true;
         }
 
 
 
     }
-
     if (isX == true) {
-
-        output = output + "x";
+        output = output + "X";
 
 
 
     } else {
-
         output = output + "0";
-
     }
-
 }
+
+//Aufgabe 09
+var sum = 0
+for (let index = 0; index < 100; index++) {
+    if (index % 2 == 0) {
+        sum += index
+    }
+}
+console.log(sum);
+
+//Aufgabe 09c
+let Colum = 5
+let output = " "
+for (let index = 0; index <= 5; index++)
+    if (index % 1 == 0)
