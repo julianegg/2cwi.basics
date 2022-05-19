1c37509fac0d50c944b8f71d92061b7c300d6f3b
Julian <julian.deronja@student.htldornbirn.at>
2022-05-19 14:44:59 +0200

    html to js

---
+++ 2cwi/JS/HTMLinJS/script10.js
@@ -0,0 +1,8 @@
+function buttonClicked() {
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parseFloat(num1) + parseInt(num2);
+    console.log(result);
+}
---
+++ 2cwi/JS/HTMLinJS/script9.js
@@ -0,0 +1,3 @@
+let element = document.getElementById("content")
+element.innerHTML = "Tolle Sache"
+console.log(element);
