4db94a77b6b1a4ed563110ce5ae13b074e8c3bb8
Julian <julian.deronja@student.htldornbirn.at>
2022-06-09 13:12:07 +0200

    01

--- 2cwi/JS/HTMLinJS/script9.js
+++ 2cwi/JS/HTMLinJS/script9.js
@@ -1,3 +1,11 @@
-let element = document.getElementById("content")
-element.innerHTML = "Tolle Sache"
-console.log(element);
+function buttonClicked() {
+    console.log("clicked")
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let result = parseFloat(num1) + parseFloat(num2);
+    console.log(result);
+    let a = parseFloat(num1) * parseFloat(num2);
+    console.log(a)
+    let c = parseFloat(num1) / parseFloat(num2);
+    console.log(c)
+}
